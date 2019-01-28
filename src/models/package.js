3f787cb8dbc9024524b8592f86b43ad58e740975
polokang <polokang@163.com>
2019-01-28 23:57:11 +1000

    recoding parse function

--- src/models/package.js
+++ src/models/package.js
@@ -31,27 +31,54 @@ function getPackageData(url, params, reqType) {
 
 function handleResponseData(responseData, companyName) {
   // console.log(responseData);
-  var $ = cheerio.load(responseData);
-  let arr = $("td");
-  var dataTemp = [];
+  // let $ = cheerio.load(responseData);
+  // let arr = $("td");
+  // let dataTemp = [];
   if (companyName === "FG") {
-    arr.each(function(k, v) {
-      var src = v.firstChild.firstChild.data;
-      dataTemp.push(src);
-    });
-    return dataTemp;
+    return exp_fg(responseData);
   }
 
-  if (companyName === "ARK") {
-    arr = $(".m-table1 tbody tr");
-    arr.each(function(k, v) {
-      var time = v.next.firstChild.data;
-      var state = v.next.tagName;
-      console.log(state);
-      dataTemp.push(time);
-    });
-    return dataTemp;
-  }
+  // if (companyName === "ARK") {
+  //   arr = $(".m-table1 > tbody > tr");
+  //   arr.removeClass;
+  //   arr.each(function(k, v) {
+  //     let td_node = v.firstChild.next;
+  //     if (td_node.tagName === "td") {
+  //       if (td_node.firstChild != null) {
+  //         let time_node = td_node.firstChild.next.firstChild;
+  //         let time = time_node.data;
+  //         let state_node = td_node.next.next.firstChild.firstChild;
+  //         console.log(time);
+  //         console.log(state_node.data);
+  //         dataTemp.push(time);
+  //       }
+  //     }
+  //   });
+  // return dataTemp;
+  // }
 
   return responseData;
+}
+
+function exp_fg(responseData) {
+  let $ = cheerio.load(responseData);
+  let arr = $("td");
+  let dataTemp = [];
+  arr.each(function(k, v) {
+    if (k % 2 === 0) {
+      //even number
+      let time = v.firstChild.firstChild.data;
+      let info = { time: time, state: "" };
+      if (k > 0) {
+        dataTemp[k - 1] = info;
+      } else {
+        dataTemp[k] = info;
+      }
+    } else {
+      //odd number
+      let state = v.firstChild.firstChild.data;
+      dataTemp[k - 1].state = state;
+    }
+  });
+  return dataTemp;
 }
